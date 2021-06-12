7d6bbbb9ffb2447dbb6f7d3cc12eb9385b6c0b4e
kinho <kinhong99@gmail.com>
2021-06-12 15:15:57 -0400

    initial commit

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Social Site</title>
+        <meta name="keywords" content="Social Site" />
+      </head>
+      <h1>Hello</h1>
+    </div>
+  );
+}
