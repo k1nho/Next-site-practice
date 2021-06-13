e99cf59ff0e55d537047c9911748815bb7bfc656
kinho <kinhong99@gmail.com>
2021-06-13 16:39:17 -0400

    wrap all components around the provider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 export default MyApp;
