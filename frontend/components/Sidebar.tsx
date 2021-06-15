64fefeefc70798f12bfe48f62e189905135df72e
kinho <kinhong99@gmail.com>
2021-06-15 18:59:51 -0400

    create sidebar component

---
+++ frontend/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import {
+  ChevronDownIcon,
+  ShoppingBagIcon,
+  UserGroupIcon,
+} from "@heroicons/react/outline";
+import {
+  CalendarIcon,
+  ClockIcon,
+  DesktopComputerIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
+import { SidebarRow } from "./SidebarRow";
+
+export const Sidebar: React.FC = () => {
+  return (
+    <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+      <SidebarRow Icon={UsersIcon} title="Friends" />
+      <SidebarRow Icon={UserGroupIcon} title="Groups" />
+      <SidebarRow Icon={ShoppingBagIcon} title="Marketplace" />
+      <SidebarRow Icon={DesktopComputerIcon} title="Watch" />
+      <SidebarRow Icon={CalendarIcon} title="Events" />
+      <SidebarRow Icon={ClockIcon} title="Memories" />
+      <SidebarRow Icon={ChevronDownIcon} title="See More" />
+    </div>
+  );
+};
