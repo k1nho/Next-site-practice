29297d07c0de1e2434104b2151522c14a6f0962b
kinho <kinhong99@gmail.com>
2021-06-16 20:16:44 -0400

    add card section for feed

---
+++ frontend/components/StoryCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+interface Iprops {
+  name: string;
+  src: string;
+  profile: string;
+}
+
+export const StoryCard: React.FC<Iprops> = ({ name, src, profile }) => {
+  return (
+    <div className="relative h-14 w-14 md:h-20 md:w-20 lg:h-52 lg:w-32 cursor-pointer overflow-x p-3 transition duration-200 transform ease-in-out hover:scale-105 hover:animate-pulse">
+      <Image
+        src={profile}
+        height={40}
+        width={40}
+        layout="fixed"
+        objectFit="cover"
+        className="absolute opacity-0 lg:opacity-100 rounded-full z-50 top-10"
+      />
+      <Image
+        src={src}
+        layout="fill"
+        className="object-cover filter brightness-75 rounded-full lg:rounded-3xl"
+      />
+    </div>
+  );
+};
