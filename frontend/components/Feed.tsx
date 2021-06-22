ee313b9e6d66f6c1b3a1983042d6f28fdc7ab7e3
kinho <kinhong99@gmail.com>
2021-06-22 19:32:29 -0400

    added hidden scrollbar

--- frontend/components/Feed.tsx
+++ frontend/components/Feed.tsx
@@ -5,18 +5,26 @@ import { Stories } from "./Stories";
 interface Iprops {
   userImg: string;
   username: string;
+  posts: {
+    timestamp: null;
+    id: string;
+    name: string;
+    message: string;
+    image: string;
+    postImage: string;
+  }[];
 }
 
-export const Feed: React.FC<Iprops> = ({ username, userImg }) => {
+export const Feed: React.FC<Iprops> = ({ username, userImg, posts }) => {
   return (
-    <div className="flex flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+    <div className="flex flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide">
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         {/* Top Stories */}
         <Stories />
         {/* Input Bar */}
         <InputBar userImg={userImg} username={username} />
         {/* Posts */}
-        <Posts />
+        <Posts posts={posts} />
       </div>
     </div>
   );
