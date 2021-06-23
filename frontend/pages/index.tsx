126a33a1c05ba6eca4515f52d95ffd3394b34868
kinho <kinhong99@gmail.com>
2021-06-23 19:16:55 -0400

    changed meta-tag

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,30 +1,40 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { getSession } from "next-auth/client";
+import Head from "next/head";
 import { Feed } from "../components/Feed";
 import { Header } from "../components/Header";
 import { Login } from "../components/Login";
 import { Sidebar } from "../components/Sidebar";
+import { Widgets } from "../components/Widgets";
+import { db } from "../firebase";
 
 export default function Home({
   session,
+  posts,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   if (!session) {
     return <Login />;
   }
+
   return (
     <div className="h-screen bg-gray-100 overflow-hidden">
-      <head>
-        <title>Social Site</title>
+      <Head>
+        <title>Discodify</title>
         <meta name="keywords" content="Social Site" />
-      </head>
+      </Head>
       {/* Header */}
       <Header username={session.user.name} userImg={session.user.image} />
       <main className="flex">
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
-        <Feed userImg={session.user.image} username={session.user.name} />
+        <Feed
+          userImg={session.user.image}
+          username={session.user.name}
+          posts={posts}
+        />
         {/* Widget */}
+        <Widgets />
       </main>
     </div>
   );
@@ -34,9 +44,21 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   //Get user
   const session = await getSession(context);
 
+  // prefetch posts on server
+  const posts = await db.collection("posts").orderBy("timestamp", "desc").get();
+
+  const docs = posts.docs.map((post) => ({
+    id: post.id,
+    name: post.data().name,
+    message: post.data().message,
+    image: post.data().image,
+    timestamp: null,
+  }));
+
   return {
     props: {
       session,
+      posts: docs,
     },
   };
 };
