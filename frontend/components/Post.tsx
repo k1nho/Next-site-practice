92b3c517c54d2981c2f0bf61cdc2ce33fd1d71fd
kinho <kinhong99@gmail.com>
2021-06-24 15:01:06 -0400

    server side rendering for posts

--- frontend/components/Post.tsx
+++ frontend/components/Post.tsx
@@ -1,16 +1,15 @@
+import { ChatAltIcon, ShareIcon, ThumbUpIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 
 interface Iprops {
-  key: string;
   name: string;
   message: string;
   image: string;
-  postImage: string;
+  postImage?: string;
   timestamp: any;
 }
 
 export const Post: React.FC<Iprops> = ({
-  key,
   name,
   message,
   image,
@@ -30,9 +29,13 @@ export const Post: React.FC<Iprops> = ({
           />
           <div>
             <p className="font-medium">{name}</p>
-            <p className="text-xs text-gray-400">
-              {new Date(timestamp?.toDate()).toLocaleString()}
-            </p>
+            {timestamp ? (
+              <p className="text-xs text-gray-400">
+                {new Date(timestamp?.toDate()).toLocaleString()}
+              </p>
+            ) : (
+              <p className="text-xs text-gray-400">Loading ...</p>
+            )}
           </div>
         </div>
         <p className="pt-4">{message}</p>
@@ -42,6 +45,21 @@ export const Post: React.FC<Iprops> = ({
           <Image src={postImage} objectFit="cover" layout="fill"></Image>
         </div>
       )}
+      {/* Footer */}
+      <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <ThumbUpIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none">
+          <ChatAltIcon className="h-4" />
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-xs sm:text-base">Share</p>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/components/Posts.tsx
+++ frontend/components/Posts.tsx
@@ -2,22 +2,42 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
 import { Post } from "./Post";
 
-export const Posts: React.FC = () => {
+interface Iprops {
+  posts: {
+    timestamp: null;
+    id: string;
+    name: string;
+    message: string;
+    image: string;
+  }[];
+}
+
+export const Posts: React.FC<Iprops> = ({ posts }) => {
   const [realtimePosts] = useCollection(
     db.collection("posts").orderBy("timestamp", "desc")
   );
   return (
     <div>
-      {realtimePosts?.docs.map((post) => (
-        <Post
-          key={post.id}
-          name={post.data().name}
-          message={post.data().message}
-          timestamp={post.data().timestamp}
-          image={post.data().image}
-          postImage={post.data().postImage}
-        />
-      ))}
+      {realtimePosts
+        ? realtimePosts?.docs.map((post) => (
+            <Post
+              key={post.id}
+              name={post.data().name}
+              message={post.data().message}
+              timestamp={post.data().timestamp}
+              image={post.data().image}
+              postImage={post.data().postImage}
+            />
+          ))
+        : posts.map((post) => (
+            <Post
+              key={post.id}
+              name={post.name}
+              message={post.message}
+              timestamp={post.timestamp}
+              image={post.image}
+            />
+          ))}
     </div>
   );
 };
